a28035fb00609203cc17c321cd52726f759d4a54
William <waortiz@hotmail.com>
2018-02-03 11:16:33 -0500

    Ajustes de seguridad.

--- src/java/co/edu/fnsp/spv/utilidades/CookieUtil.java
+++ src/java/co/edu/fnsp/spv/utilidades/CookieUtil.java
@@ -6,12 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
-    public static void create(HttpServletResponse httpServletResponse, String name, String value, Boolean secure, Integer maxAge, String domain) {
+    public static void create(HttpServletResponse httpServletResponse, String name, String value, Boolean secure, Integer maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setSecure(secure);
         cookie.setHttpOnly(true);
         cookie.setMaxAge(maxAge);
-        cookie.setDomain(domain);
         cookie.setPath("/");
         httpServletResponse.addCookie(cookie);
     }
